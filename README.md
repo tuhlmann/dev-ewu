6a398ea0a393ed1d870c467d956ca75ec24dee92
Torsten Uhlmann <tuhlmann@agynamix.de>
2013-10-30 08:35:40 +0100

    Base System

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ dev-ewu
 =======
 
 A Repository holding scripts and settings to bootstrap and setup a developer Ubuntu machine using Ansible
+
+Install
+=======
+
+    wget -O - https://raw.github.com/tuhlmann/dev-ewu/master/ansible-bootstrap.sh | sh
+
+
--- ansible-bootstrap.sh
+++ ansible-bootstrap.sh
@@ -1,9 +1,22 @@
 #!/bin/sh
 
+# Install latest updates
 sudo apt-get update
 sudo apt-get upgrade -y
 
+Add Ansible repository
 sudo add-apt-repository -y ppa:rquillo/ansible
 sudo apt-get update
-sudo apt-get install -y ansible
+
+# Install Ansible and git to clone the repository
+sudo apt-get install -y ansible git-core git
+
+pushd $HOME
+
+git clone git@github.com:tuhlmann/dev-ewu.git
+
+cd dev-ewu/ansible
+ansible-playbook -i hosts/local dev-ewu.yml
+
+popd
 
